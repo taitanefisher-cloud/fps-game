5f1389c050a86875c8c69470c9804662d59565c7
taitanefisher-cloud <tai.tane.fisher@gmail.com>
2026-03-25 17:49:03 +1100

    fixes for gltf

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js";
+
 // ===== SETUP =====
 let scene, camera, renderer;
 let zombies = [];
@@ -89,7 +92,8 @@ function init() {
   }
 
   // gun model
-  const loader = new THREE.GLTFLoader();
+  const loader = new GLTFLoader();
+
   loader.load(
     "https://threejs.org/examples/models/gltf/DamagedHelmet/glTF/DamagedHelmet.gltf",
     (gltf)=>{
